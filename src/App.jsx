b9df35a38b53c7259e1832f5323e2cfaa2c301d3
Bryniuk Valia <valyanychyporuk@gmail.com>
2024-02-26 00:53:51 +0200

    - completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
-import Header from "./components/Header/Header.jsx";
-import Sidebar from "./components/Sidebar/Sidebar.jsx";
-import Content from "./components/Content/Content.jsx";
-import style from './App.module.css';
+import Card from "./components/Card/Card.jsx";
 
 function App() {
+
+  const cardBtnClickHandler = (cardProps) => {
+    alert(`Hello from card: ${cardProps.title}`);
+  }
+
   return (
     <>
-      <Header className={style.header} />
-      <Sidebar className={style.sidebar} />
-      <Content className={style.content}>
-        <>
-          <h1>Lorem Ipsum</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare lacus vel sem lobortis, vel aliquam lectus interdum. Quisque bibendum felis et tempus aliquet. Aliquam erat volutpat. Donec a vestibulum nibh. Mauris eros enim, interdum eget sodales hendrerit, posuere porttitor libero. Sed ut sem ante. Nullam a urna augue. Sed euismod ipsum arcu, ac suscipit libero laoreet a. Vivamus rhoncus id dolor quis porttitor. Pellentesque in commodo lorem. Phasellus consequat nibh quis purus porta semper. Maecenas ex velit, hendrerit et eleifend non, faucibus fermentum sem. Duis vel sagittis nibh, eget suscipit mauris. Suspendisse ac rutrum urna, et interdum nunc.
-          </p>
-          <p>
-            Cras tincidunt nunc sit amet sem luctus pulvinar. Vestibulum aliquet convallis risus id vehicula. Nam aliquam massa libero, vel fringilla risus lacinia quis. Mauris varius neque vel purus sollicitudin faucibus. Nunc tempus, ex id gravida facilisis, diam lacus consectetur turpis, at auctor nibh ipsum eget tortor. Duis bibendum varius massa, eget pharetra lectus pulvinar id. Suspendisse ornare et elit dignissim vulputate. Ut a tellus fringilla, interdum massa quis, volutpat ligula. Phasellus tristique imperdiet venenatis.
-          </p>
-        </>
-      </Content>
+      <Card title="Card title" text="Some quick example text to build on the card" btnText="Go somewhere"  onClick={cardBtnClickHandler} />
+      <Card title="Card title 2" text="Some quick example text to build on the card 2" btnText="Go somewhere"  onClick={cardBtnClickHandler} />
+      <Card title="Card title 3" text="Some quick example text to build on the card 3" btnText="Go somewhere"  onClick={cardBtnClickHandler} />
     </>
   )
 }
