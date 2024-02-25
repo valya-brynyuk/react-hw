a23b6fc42deec000157879a951bf5c5bf9ed1c78
Bryniuk Valia <valyanychyporuk@gmail.com>
2024-02-26 01:04:04 +0200

    - completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import Card from "./components/Card/Card.jsx";
 
 function App() {
 
-  const cardBtnClickHandler = (cardProps) => {
-    alert(`Hello from card: ${cardProps.title}`);
-  }
-
   return (
     <>
-      <Card title="Card title" text="Some quick example text to build on the card" btnText="Go somewhere"  onClick={cardBtnClickHandler} />
-      <Card title="Card title 2" text="Some quick example text to build on the card 2" btnText="Go somewhere"  onClick={cardBtnClickHandler} />
-      <Card title="Card title 3" text="Some quick example text to build on the card 3" btnText="Go somewhere"  onClick={cardBtnClickHandler} />
+      <Card title="Card title"/>
+      <Card text="Some quick example text to build on the card 2"/>
+      <Card title="Card title 3" text="Some quick example text to build on the card 3"/>
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,29 +2,13 @@ import React from "react";
 import style from "./Card.module.css";
 
 class Card extends React.Component {
-  btnClickHandler = (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-
-    if (!this.props.onClick) {
-      return;
-    } else if (typeof this.props.onClick !== 'function') {
-      throw new Error('Card on Click handler must be a function')
-    }
-
-    this.props.onClick({
-      title: this.props.title,
-      text: this.props.text,
-    });
-  }
 
   render() {
     return (
       <div className={style.card}>
         <div className={style.body}>
-          <h4 className={style.title}>{this.props.title}</h4>
-          <p className={style.text}>{this.props.text}</p>
-          <button type="button" className={style.button} onClick={this.btnClickHandler}>{this.props.btnText}</button>
+          {this.props.title?.length ? <h4 className={style.title}>{this.props.title}</h4> : null}
+          {this.props.text?.length ? <p className={style.text}>{this.props.text}</p> : null}
         </div>
       </div>
     );
