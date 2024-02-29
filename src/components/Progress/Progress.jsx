429b567ee16d5ed886f8d9fa56eb505a6080207f
Bryniuk Valia <valyanychyporuk@gmail.com>
2024-02-29 23:17:19 +0200

    - completed

--- src/components/Progress/Progress.jsx
+++ src/components/Progress/Progress.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 
 class Progress extends React.Component {
+  constructor() {
+    super();
+
+    if (!Number.isFinite(this.props.percentage)) {
+      throw new Error('Percentage must be a valid number');
+    } else if (this.props.percentage < 0) {
+      throw new Error('Percentage must be greater than 0');
+    } else if (this.props.percentage > 100) {
+      throw new Error('Percentage must be less than 100');
+    }
+  }
 
   render() {
     return (
